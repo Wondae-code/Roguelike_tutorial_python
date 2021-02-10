7d4ac8bc1e7422ddc83212038fe8ba3e25602da1
Wondae <48678910+Wondae-code@users.noreply.github.com>
2021-02-10 23:22:44 +0900

    part1
    part1 completed

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-import tcod as libtcod
+import tcod
+
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main() -> None:
   screen_width = 80
   screen_height = 50
 
-  tileset = libtcod.tileset.load_tilesheet("sprite.png", 32, 8, libtcod.tileset.CHARMAP_TCOD)
+  player_x = int(screen_width / 2)
+  player_y = int(screen_height / 2)
 
-  with libtcod.context.new_terminal(screen_width, 
+  tileset = tcod.tileset.load_tilesheet("sprite.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+  event_handler = EventHandler()
+
+  with tcod.context.new_terminal(screen_width, 
   screen_height, 
   tileset=tileset, title="Yet Another Roguelike Tutorial",
   vsync=True,
   ) as context:
-    root_console = libtcod.Console(screen_width, screen_height, order="F")
+    root_console = tcod.Console(screen_width, screen_height, order="F")
     while True:
-      root_console.print(x=1,y=1,string="@")
+      root_console.print(x=player_x,y=player_y,string="@")
 
       context.present(root_console)
 
-      for event in libtcod.event.wait():
-        if event.type == "QUIT:
+      root_console.clear()
+
+      for event in tcod.event.wait():
+        action = event_handler.dispatch(event)
+
+        if action is None:
+          continue
+        if isinstance(action, MovementAction):
+          player_x += action.dx
+          player_y += action.dy
+
+        elif isinstance(action, EscapeAction):
           raise SystemExit()
 
 if __name__ == "__main__":
